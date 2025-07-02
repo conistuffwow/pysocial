426f0b8711be3bcbe6b863bef70aae0decdd00fa
conistuffwow <deatu013@gmail.com>
2025-07-02 16:06:03 -0400

    test commit

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from . import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
-    password = db.Column(db.string(128), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
     posts = db.relationship('Post', backref='user', lazy=True)
 
 class Post(db.Model):
--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
+    print(app.template_folder)
     app.run(debug=True)
